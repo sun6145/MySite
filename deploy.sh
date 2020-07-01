260133dffdd036d743b9c8c9aa214a3ae76de462
sun6145 <1546369127@qq.com>
2020-07-02 00:59:59 +0800

    添加配置主题

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
+rm -rf public
 hugo
 cd public
 git init
-git remote add origin https://github.com/sun6145/MySite.git
+git remote add origin git@github.com:sun6145/MySite.git
 git add -A
 git commit -m "deploy by hugo"
-git push -u origin master
+git push -u origin master -f
