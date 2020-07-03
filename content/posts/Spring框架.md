876daed2a4e17fd42052aa690ea7e5f6d534571f
sun6145 <1546369127@qq.com>
2020-07-03 09:03:36 +0800

    添加新文章

---
+++ content/posts/Spring框架.md
@@ -0,0 +1,10 @@
+---
+title: "Spring框架"
+date: 2020-07-02T21:04:09+08:00
+draft: false
+---
+
+## Spirng 概述
+
+
+
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ hugo
 cd public
 git init
 git remote add origin git@github.com:sun6145/MySite.git
+git remote add coding git@e.coding.net:sun__shine/hugo.git
 git add -A
 git commit -m "deploy by hugo"
 git push -u origin master -f
+git push -u coding master -f
